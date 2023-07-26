3c9efd0b3f0b19d95e3e5a6cb37ea1aff331598d
Deaath65 <deaath65@gmail.com>
2023-07-26 12:23:54 +0545

    Models finished

--- durbean/accounts/models.py
+++ durbean/accounts/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone, name, email, photo, password=None, **extra_fields):
+        if not phone:
+            raise ValueError("The Phone field must be set")
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        user = self.model(phone=phone, name=name, email=email, photo=photo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, name, email, photo, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(phone, name, email, photo, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(unique=True, max_length=20)
+    photo = models.ImageField(upload_to='user_photos/', null=True, blank=True)
+
+    # Additional fields if needed
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['name', 'email', 'photo']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.name
--- durbean/durbean/settings.py
+++ durbean/durbean/settings.py
@@ -70,6 +70,9 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
