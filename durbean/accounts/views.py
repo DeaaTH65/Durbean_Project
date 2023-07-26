3015c4442341dace7dce00419b313fb1cfec61db
Deaath65 <deaath65@gmail.com>
2023-07-26 13:52:49 +0545

    login user

--- durbean/accounts/views.py
+++ durbean/accounts/views.py
@@ -10,7 +10,19 @@ def home(request):
 
 
 def loginPage(request):
-    pass
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You Have Been Logged In!")
+			return redirect('home')
+		else:
+			messages.success(request, "There Was An Error Logging In, Please Try Again...")
+			return redirect('home')
+	else:
+		return render(request, 'login.html')
 
 
 def logoutUser(request):
