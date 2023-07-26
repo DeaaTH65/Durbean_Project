9a2c69e86304a38d89ba4f3c87acdb4a49dff713
Deaath65 <deaath65@gmail.com>
2023-07-26 14:15:00 +0545

    register user

---
+++ durbean/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'name', 'phone', 'photo', 'username',) 
+
+        
+        
+
--- durbean/accounts/views.py
+++ durbean/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def home(request):
@@ -32,4 +32,12 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    pass
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
