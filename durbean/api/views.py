b4d1a967ef7deec335315ef8dd6b28275d20fb1f
Deaath65 <deaath65@gmail.com>
2023-07-27 11:13:01 +0545

    rest framework view

--- durbean/api/views.py
+++ durbean/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from accounts.models import CustomUser
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    
